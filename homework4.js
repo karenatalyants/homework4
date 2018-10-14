be050959b0fe9535134c87e3dda8c0b5bcd787e0
karenatalyants <karenatalyants@gmail.com>
2018-10-15 00:52:26 +0400

    homework 4

--- homework4.js
+++ homework4.js
@@ -1,8 +1,6 @@
 
-//this function takes a number, and prints that many * next to
+//this function takes a number, and prints that many stars * next to
 //each other on a line
-
-
 const printMany = function (b) {
   let count = b;
   let stars = '*';
@@ -13,16 +11,43 @@ const printMany = function (b) {
   };
   return stars;
 
+//this function takes the given height of the upside-down triangle
+//and uses the previous function as well as adds correct amount of spaces
+//before every line after the first
+
 }
-
-
 const triangleStars = function (a) {
-  let triangleHeight = a;
-  while(triangleHeight>1) {
-    triangleHeight = triangleHeight-1;
-    return printMany(triangleHeight);
 
-  }
+//below calculating the length of row and creating a space which at first
+// is an empty string (we will later make it a valid space)
+let rowLength = a*2-1;
+let space = '';
+
+//below is the while loop, which checks the rowlength, and based on that
+//uses the first function to print triangle rows with spaces
+while (rowLength>0) {
+  console.log(space+printMany(rowLength));
+  rowLength = rowLength-2;
+  space = space + ' ';
+
 }
+};
 
-console.log(triangleStars(7));
+triangleStars(27);
+
+
+//a function that takes a two dimensional array and returns
+//a single array with all the values from the child arrays
+
+const multiToSingle = function ([1,2,3],[4,5,6],[7,8,9]) {
+  const arr = [];
+  let n = arr.length-1;
+  let m = a.length-1;
+
+  while (n>=0) {
+    arr[n] = a[m];
+    n=n-1;
+    console.log(arr);
+  }
+
+}
