d1218bbc069197aba2708f154128b94362f970e3
karenatalyants <karenatalyants@gmail.com>
2018-10-17 00:47:52 +0400

    whatever

--- homework4.js
+++ homework4.js
@@ -39,15 +39,54 @@ triangleStars(27);
 //a function that takes a two dimensional array and returns
 //a single array with all the values from the child arrays
 
-const multiToSingle = function ([1,2,3],[4,5,6],[7,8,9]) {
-  const arr = [];
+const multiToSingle = function ([[1,2,3],[4,5,6],[7,8,9]]) {
+  const newArr = [whatever];
   let n = arr.length-1;
   let m = a.length-1;
 
-  while (n>=0) {
-    arr[n] = a[m];
-    n=n-1;
-    console.log(arr);
-  }
+};
 
+
+// this function finds the largest or the smallest number from an
+// array of ANY length
+
+const findMinMax = function (a, b) {
+
+  if (b === true) {
+
+  const arr = a;
+
+  // below I am finding the index number of the last number in the array
+
+  let n = arr.length-1;
+  let m = arr.length-1;
+  let desNumber = arr[n]; // desNumber is the "desired number"
+  while (n>=0) {
+    // comparing the last number to all the numbers in the array below
+    while (arr[n]>arr[m] && m>=0) {
+      m = m-1;
+      desNumber = arr[n]
+    };
+    //below I am switching to another number if the requirement is not met
+    n = n - 1;
+  }
+  return 'the largest number is ' + desNumber;
+} else if (b === false) {
+
+
+  const arr = a;
+  let n = arr.length-1;
+  let m = arr.length-1;
+  let desNumber = arr[n];
+  while (n>=0) {
+    while (arr[n]<arr[m] && m>=0) {
+      m = m-1;
+      desNumber = arr[n]
+    };
+    n = n - 1;
+  }
+  return 'the smallest number is ' + desNumber;
 }
+}
+
+ console.log(findMinMax([1,5,3,2,7,22,2], false));
